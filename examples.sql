adef1f189baf237ca51350b8f2a3a39568a025f5
Sobitkhon-1 <ksobitkhon.@gmail.com>
2025-09-23 21:56:11 +0500

    qqq

--- examples.sql
+++ examples.sql
@@ -1,7 +1,7 @@
 -- SHOW ALL DATABASES
-SELECT datname as name FROM pg_database;
+SELECT datname AS name FROM pg_database;
 
--- CREATE NEW DATABAE
+-- CREATE NEW DATABASE
 CREATE DATABASE my_app;
 
 -- DROP DATABASE
@@ -9,34 +9,29 @@ DROP DATABASE my_app;
 
 -- CREATE TABLE
 CREATE TABLE users (
-	id int,
-	username varchar( 32 ),
-	gender boolean
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(32) NOT NULL,
+    gender BOOLEAN NOT NULL
 );
 
 -- SELECT ALL DATA FROM TABLE
-select * from users;
+SELECT * FROM users;
 
 -- SELECT SPECIFIC COLUMNS FROM TABLE
-select id, username from users;
+SELECT id, username FROM users;
 
 -- INSERT SINGLE DATA (ROW) TO TABLE
-INSERT INTO
-	users ( age )
-VALUES
-	( 22 )
-;
+INSERT INTO users (username, gender)
+VALUES ('sobit', true);
 
 -- INSERT MULTIPLE DATA (ROWS) TO TABLE
-INSERT INTO
-	users ( id, username, gender )
+INSERT INTO users (username, gender)
 VALUES
-	( 1, 'najimov', true ),
-	( 2, 'firdavs', true ),
-	( 3, 'alisher', true ),
-	( 4, 'javohir', true ),
-	( 5, 'asror', true ),
-	( 6, 'zarnigor', false ),
-	( 7, 'dilfuzaxon', false ),
-	( 8, 'muhammadrahim', true )
-;
+    ('najimov', true),
+    ('firdavs', true),
+    ('alisher', true),
+    ('javohir', true),
+    ('asror', true),
+    ('zarnigor', false),
+    ('dilfuzaxon', false),
+    ('muhammadrahim', true);
